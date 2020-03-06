7b3a61927748db99b241989627482c5cb6350d33
Artem Sergeev <aesergnsk@gmail.com>
2020-03-06 15:15:59 +0700

    added filtering via area field value

--- TagLinkedWorkItems/scripts/index.ts
+++ TagLinkedWorkItems/scripts/index.ts
@@ -1,66 +1,76 @@
 import * as tl from 'azure-pipelines-task-lib/task';
-import * as request from "request-promise-native";
+import * as request from 'request-promise-native';
 
-const collectionUrl = process.env["SYSTEM_TEAMFOUNDATIONCOLLECTIONURI"];
-const teamProject = process.env["SYSTEM_TEAMPROJECT"];
+const collectionUrl = process.env['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI'];
+const teamProject = process.env['SYSTEM_TEAMPROJECT'];
 const accessToken = tl.getEndpointAuthorization('SystemVssConnection', true).parameters.AccessToken;
 
 async function run() {
     try {
         const pipelineType = tl.getInput('pipelineType');
-        const workItemsData = pipelineType === "Build" ?
-            await getWorkItemsFromBuild() :
-            await getWorkItemsFromRelease();
+
+        const workItemsData = pipelineType === 'Build' ? await getWorkItemsFromBuild() : await getWorkItemsFromRelease();
         workItemsData.forEach(async (workItem: any) => {
             await addTagToWorkItem(workItem);
         });
-    }
-    catch (err) {
+    } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
 
 async function getWorkItemsFromBuild() {
-    const buildId = process.env["BUILD_BUILDID"];
+    const buildId = process.env['BUILD_BUILDID'];
     const uri = `${collectionUrl}/${teamProject}/_apis/build/builds/${buildId}/workitems`;
     const options = createGetRequestOptions(uri);
     const result = await request.get(options);
+    tl.logIssue(tl.IssueType.Warning, `result: ${JSON.stringify(result)}`);
     return result.value;
-
 }
 
 async function getWorkItemsFromRelease() {
-    const releaseId = process.env["RELEASE_RELEASEID"];
+    const releaseId = process.env['RELEASE_RELEASEID'];
     const uri = `${collectionUrl}/${teamProject}/_apis/release/releases/${releaseId}/workitems`;
     const options = createGetRequestOptions(uri);
     const result = await request.get(options);
     return result.value;
-
 }
 
 async function addTagToWorkItem(workItem: any) {
-    const tagFromInput = tl.getInput('tagToAdd');
-    const uri = workItem.url + "?fields=System.Tags&api-version=2.0";
-    const getOptions = createGetRequestOptions(uri)
+    const uri = workItem.url + '?api-version=5.1';
+    const getOptions = createGetRequestOptions(uri);
     const result = await request.get(getOptions);
-    const currentTags = result.fields['System.Tags'];
+    const workItemArea = tl.getInput('workItemArea');
+    if (workItemArea !== null) {
+        const currentWorkItemArea = result.fields['System.AreaPath'];
+        if (currentWorkItemArea === workItemArea) {
+            await updateWorkItemTags(result);
+        }
+    } else {
+        await updateWorkItemTags(result);
+    }
+}
+
+async function updateWorkItemTags(workItem: any) {
+    const tagFromInput = tl.getInput('tagToAdd');
+    const uri = workItem.url + '?api-version=5.1';
+    const currentTags = workItem.fields['System.Tags'];
+
     let newTags = '';
     if (currentTags !== undefined) {
-        newTags = currentTags + ";" + tagFromInput;
+        newTags = currentTags + ';' + tagFromInput;
     } else {
         newTags = tagFromInput;
     }
     const patchOptions = getPatchRequestOptions(uri, newTags);
-    await request.patch(patchOptions)
-
+    await request.patch(patchOptions);
 }
 
 function createGetRequestOptions(uri: string): any {
     let options = {
         uri: uri,
         headers: {
-            "authorization": `Bearer ${accessToken}`,
-            "content-type": "application/json"
+            authorization: `Bearer ${accessToken}`,
+            'content-type': 'application/json'
         },
         json: true
     };
@@ -71,17 +81,19 @@ function getPatchRequestOptions(uri: string, newTags: string): any {
     const options = {
         uri: uri,
         headers: {
-            "authorization": `Bearer ${accessToken}`,
-            "content-type": "application/json-patch+json"
+            authorization: `Bearer ${accessToken}`,
+            'content-type': 'application/json-patch+json'
         },
-        body: [{
-            "op": "add",
-            "path": "/fields/System.Tags",
-            "value": newTags
-        }],
+        body: [
+            {
+                op: 'add',
+                path: '/fields/System.Tags',
+                value: newTags
+            }
+        ],
         json: true
     };
-    return options
+    return options;
 }
 
 run();
---
+++ true-sergeev.work-items-tagging-task-1.0.47/extension.md
@@ -0,0 +1,22 @@
+Tag Work Items Linked With Build or Release
+===
+
+## How to use
+
+Find the task in the Utility group  
+
+![Task in utility group](https://github.com/asergeev95/AddTagToBuildRelatedTasks/raw/master/screenshots/Screenshot_1.png) 
+
+and add it to your pipeline
+
+Specify the type of pipeline of tag that will be added to linked work items 
+
+![Task setting](https://github.com/asergeev95/AddTagToBuildRelatedTasks/raw/master/screenshots/Screenshot_2.png)
+
+Enjoy it!
+
+## GitHub
+
+Any contribution or feedback is welcome!
+
+[Artem Sergeev/TagLinkedWorkItemsTask](https://github.com/asergeev95/TagLinkedWorkItemsTask)
